98fc0e87ce808e76e3bbc6cd3c10f7d23302155b
Martin <mbpster@gmail.com>
2022-12-10 18:33:22 +0100

    Added Source code

---
+++ BaseMenuWithToolKit/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BaseMenuWithToolKit;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ BaseMenuWithToolKit/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BaseMenuWithToolKit;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
